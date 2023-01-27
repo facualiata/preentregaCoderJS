2e0702d4f3f1541fcd1eefe890ba1769e633da3e
facualiata <113631169+facualiata@users.noreply.github.com>
2023-01-27 15:29:13 -0300

    Add files via upload

--- intro.js
+++ intro.js
@@ -5,10 +5,10 @@ function edad (){
     let edad = prompt("ingrese su edad ");
     if(edad <= 17  ||  edad === ""){
     while (edad <= 17 || edad === ""){
-        alert("usted es menor de edad")
+        alert("usted es menor de edad");
         edad = prompt("reingrese su edad ");
+        }
     }
-}
 }
 
 edad(); 
@@ -19,39 +19,58 @@ function usuario() {
     if (nombre == ""){
         alert("su nombre es invalido");
         while(nombre == ""){
-            nombre = prompt("reingrese su nombre")
+            nombre = prompt("reingrese su nombre");
         }
     }else{alert("bienvenido " + nombre)}
 }
 usuario();
 /**function para pedir que va llevar */
 
-class Menu { 
+class MenuCafe { 
     constructor (variedad, precio){
         this.variedad = variedad;
         this.precio = precio;
+    }
 }
-/**en las clases puedo poner metodos sin this. */
+class MenuComida {
+    constructor(variedad, precio){
+        this.variedad = variedad;
+        this.precio = precio;
+    }
 }
-const cafeSolo = new Menu ("cafeSolo", 180);
-const capuchino = new Menu ("capuchino", 190);
-const conLeche = new Menu ("conLeche", 200);
-const submarino = new Menu ("submarino", 220);
 
+const cafeSolo = new MenuCafe ("cafeSolo", 180);
+const capuchino = new MenuCafe ("capuchino", 190);
+const conLeche = new MenuCafe ("conLeche", 200);
+const submarino = new MenuCafe ("submarino", 220);
+////
 
-const listMenu = [];
+const medialunas = new MenuComida("medialunas", 30);
+const facturas = new MenuComida ("facturas", 30);
+const tostados = new MenuComida("tostados", 50);
 
-listMenu.push(cafeSolo, capuchino, conLeche, submarino
+const listMenuCafe = [];
+const listaMenuComida = [];
+
+listMenuCafe.push(cafeSolo, capuchino, conLeche, submarino
     );
 
+listaMenuComida.push(medialunas, facturas, tostados);
 
-const pedido = prompt("ingrese su pedido \n coloque 0 para pedir cafe solo \n coloque 1 para pedir capuchino \n coloque 2 para pedir cafe con leche \n coloque 3 para pedir submarino");
 
-    console.log(listMenu[pedido]);
+const pedidoCafe = prompt("ingrese su pedido \n coloque 0 para pedir cafe solo \n coloque 1 para pedir capuchino \n coloque 2 para pedir cafe con leche \n coloque 3 para pedir submarino");
 
-for(let i = 0; i < listMenu.length; i++)[
-    console.log(listMenu[i])
+    console.log(listMenuCafe[pedidoCafe]);
+
+const pedidoComida = prompt("ingrese su pedido \n coloque 0 para pedir medialunas \n coloque 1 para pedir facturas \n coloque 2 para pedir tostados");
+
+console.log(listaMenuComida[pedidoComida]);
+
+
+for(let i = 0; i < listMenuCafe.length; i++)[
+    console.log(listMenuCafe[i])
 ]
 
-
-// g
+for (let i = 0; i < listaMenuComida.length; i++)[
+    console.log(listaMenuComida[i])
+]
